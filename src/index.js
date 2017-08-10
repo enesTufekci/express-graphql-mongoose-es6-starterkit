b18a43da5f2ee4b56343270b449c5be5ead8d86b
enesTufekci <enesxtufekci@gmail.com>
2017-08-10 15:34:27 +0200

    graphql routes

--- src/index.js
+++ src/index.js
@@ -1,14 +1,24 @@
+import passport from 'passport';
+import graphqlRoutes from './graphql';
 import server from './config/server.config';
 import config from './config/app.config';
 import db from './config/db.config';
 import auth from './auth';
 
+
 server.get('/health-check', (req, res) => {
   res.send('Server is up and running!');
 });
 
-// Authentication endpoints
+// Authentication routes
 server.use('/auth', auth);
+
+// Graphql routes
+if (config.ENV === 'dev' || config.ENV === 'development') {
+  server.use(graphqlRoutes);
+} else {
+  server.use(passport.authenticate('local', { session: false }), graphqlRoutes);
+}
 
 // Throw error if database connection fails
 db.connection.on('error', () => {
