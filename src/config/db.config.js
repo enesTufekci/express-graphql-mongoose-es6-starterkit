64bcfc2cbae5280749aa4b0cebc6cddc96d6b3c1
enesTufekci <enesxtufekci@gmail.com>
2017-08-10 13:29:37 +0200

    mongoose depreciation fixes

--- src/config/db.config.js
+++ src/config/db.config.js
@@ -1,17 +1,12 @@
 import mongoose from 'mongoose';
 import config from './app.config';
 
-mongoose.createConnection(config.DB_HOST, {
-  user: config.DB_USER,
-  pass: config.DB_PASS,
-  server: {
-    socketOptions: {
-      keepAlive: 1,
-    },
-  },
+mongoose.connect(config.DB_HOST, {
+  useMongoClient: true,
+  keepAlive: 1,
+  poolSize: 2,
 });
 
-// Set mongoose promise as global promise(ES6)
 mongoose.Promise = Promise;
 
 // print mongoose logs in dev env
@@ -20,10 +15,6 @@ if (config.MONGOOSE_DEBUG) {
     console.info(`${collectionName}.${method}`, doc); // eslint-disable-line no-console
   });
 }
-
-mongoose.connection.on('error', () => {
-  throw new Error('Unable to connect to database');
-});
 
 mongoose.connection.on('connected', () => {
   console.info('Database connection successful!'); // eslint-disable-line no-console
